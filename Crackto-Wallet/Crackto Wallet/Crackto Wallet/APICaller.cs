31a4fa0d01e709dbcf0e7409f19680c75dc1917d
ThomasLuchies <Thomasluchies22@gmail.com>
2022-05-05 13:14:53 +0200

    APICaller update

--- Crackto-Wallet/Crackto Wallet/Crackto Wallet/APICaller.cs
+++ Crackto-Wallet/Crackto Wallet/Crackto Wallet/APICaller.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,7 +10,7 @@ namespace Crackto_Wallet
 {
     public class APICaller
     {
-        private string APIURL = "https://testnet.binance.vision/api";
+        private string APIURL = "https://testnet.binance.vision";
         private string APIKey = "iC8dgRNbVuc5nSvxvQPAVHhfIwKclB0hTaY2D9KaF2RhMBT8U94qjoGFtbc8LQJR";
         private string SecretKey = "IUAEy4p7ZTlY8cPCqukCCg3ACDRy09pOyxlMvJA4IdpCbBn3DKd2DSjglkShAGYf";
 
@@ -21,28 +22,63 @@ namespace Crackto_Wallet
 
         public Task GetBalance()
         {
+            
             return null;
         }
 
-        public Task GetActiveOrders()
+        public string GetActiveOrders()
         {
-            return null;
+            string endPoint = APIURL + "/api/v3/openOrders";
+            Dictionary<string, string> reqParams = new Dictionary<string, string>()
+            {
+                {"timestamp", ((int)DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1)).TotalSeconds).ToString()}
+            };
+            Task<string> result = APICall(reqParams, endPoint, true);
+
+            return result.IsCompletedSuccessfully ? result.Result : null;
         }
 
         public double GetCoinValue(CoinType coinType)
         {
-            string endPoint = APIURL + "/api/v3/ticker/price?symbol=" + coinType.ToString();
-            endPoint += coinType.ToString();
-            Task<string> result = APICall(endPoint);
+            string endPoint = APIURL + "/api/v3/ticker/price";
+            Dictionary<string, string> reqParams= new Dictionary<string, string>()
+            {
+                { "symbol", coinType.ToString() }
+            };
+            Task<string> result = APICall(reqParams, endPoint, false);
 
             return result.IsCompletedSuccessfully ? Convert.ToDouble(result.Result) : 0;
         }
 
-        public async Task<string> APICall(string endPoint)
+        public string CreateHmacSignature(string inputs)
         {
+            using (HMACSHA256 hmacsha256 = new HMACSHA256(Encoding.UTF8.GetBytes(SecretKey)))
+            {
+                var hash = hmacsha256.ComputeHash(Encoding.UTF8.GetBytes(inputs));
+                return Convert.ToBase64String(hash);
+            }
+            return null;
+        }
+
+        public async Task<string> APICall(Dictionary<string, string> reqParams, string endPoint, bool signature)
+        {
+            string paramsString = "?";
+
+            int count = 0;
+            foreach(KeyValuePair<string, string> reqParam in reqParams)
+            {
+                paramsString += reqParam.Key + "=" + reqParam.Value;
+
+                if(count != 0 || count != reqParams.Count - 1)
+                    paramsString += "&";
+            }
+
+            if(signature)
+                paramsString += "&signature=" + CreateHmacSignature(paramsString);
+
             using (var client = new HttpClient())
             {
-                HttpResponseMessage response = await client.GetAsync(endPoint);
+                HttpResponseMessage response = await client.GetAsync((endPoint + paramsString));
                 if (response.IsSuccessStatusCode)
                 {
                     return response.Content.ToString();
---
+++ Crackto-Wallet/Crackto Wallet/test/UnitTest.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+using Crackto_Wallet;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            APICaller apiCaller = new APICaller();
+            Console.WriteLine(apiCaller.GetCoinValue(CoinType.BTCBUSD));
+        }
+    }
+}
