4ffc1c2863efcdbb040510a9c4166d9d4b90e1d1
Kariemdv <56026990+Kariemdv@users.noreply.github.com>
2022-05-06 16:53:57 +0200

    Fixed conflict, added Order page

--- Crackto-Wallet/Crackto Wallet/Crackto Wallet/Screens/OrderPage.xaml.cs
+++ Crackto-Wallet/Crackto Wallet/Crackto Wallet/Screens/OrderPage.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Crackto_Wallet.Orders;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -26,22 +28,74 @@ namespace Crackto_Wallet.Screens
 
     public sealed partial class OrderPage : Page
     {
+        private APICaller apiCaller = new APICaller();
+
         public OrderPage()
         {
             this.InitializeComponent();
         }
         private void Page_Loaded(object sender, RoutedEventArgs e)
         {
-            AddCoinTypes();
+            AddDropdownValues();
+            AddAllOrders();
         }
 
-        private void AddCoinTypes()
+        private void AddDropdownValues()
         {
             foreach (var coinType in Enum.GetValues(typeof(CoinType)))
             {
-                CoinType.Items.Add(coinType);
+                AllCoinTypesDropdownMarket.Items.Add(coinType);
+                AllCoinTypesDropdownLimit.Items.Add(coinType);
             }
-            
+
+            foreach (var value in Enum.GetValues(typeof(OrderType)))
+            {
+                BuySellDropdownMarket.Items.Add(value);
+                BuySellDrowndownLimit.Items.Add(value);
+            }
+
+            foreach (var marketType in Enum.GetValues(typeof(MarketOrderType)))
+                MarketTypeDropdown.Items.Add(marketType);
+        }
+
+        private void AddAllOrders()
+        {
+            string JSONResponse = apiCaller.GetActiveOrders();
+            if (JSONResponse == null)
+                return;
+
+            Debug.WriteLine("JSON Response: ", JSONResponse);
+            Nancy.Json.JavaScriptSerializer serializer = new Nancy.Json.JavaScriptSerializer();
+            Orders orders = serializer.Deserialize<Orders>(JSONResponse);
+
+            foreach (var order in orders.orders)
+                ActiveOrders.Items.Add(order);
+
+        }
+
+        public class Orders
+        {
+            public List<Order> orders = new List<Order>();
+        }
+
+        public class Order
+        {
+            public string symbol;
+            public int orderId;
+            public string type;
+            public string side;
+            public string price;
+            public string time;
+        }
+
+        private void Button_Click_Market(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_Limit(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
