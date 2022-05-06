e3358bf4ca20e1ac0569aa7cfdc1ce3bf6990288
Marvin Hoffenkamp <marvin.hoffenkamp@student.nhlstenden.com>
2022-05-06 15:41:59 +0200

    add read write logic for api keys

--- Crackto-Wallet/Crackto Wallet/Crackto Wallet/Screens/LoginPage.xaml.cs
+++ Crackto-Wallet/Crackto Wallet/Crackto Wallet/Screens/LoginPage.xaml.cs
@@ -12,6 +12,11 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Crackto_Wallet.FileManager;
+using Newtonsoft.Json;
+using Windows.UI.Popups;
+using Nancy.Json;
+using System.Diagnostics;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -37,9 +42,36 @@ namespace Crackto_Wallet.Screens
 
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private async void Button_Click(object sender, RoutedEventArgs e)
         {
+            String APIKEY = apikey.Text;
+            String SECRETKEY = secretkey.Text;
 
+            Keys keys = new Keys
+            {
+                apikey = APIKEY,
+                secretkey = SECRETKEY,
+            };
+
+            JavaScriptSerializer serializer = new JavaScriptSerializer();
+            string json = serializer.Serialize(keys);
+            FileManagerJSON fileManager = new FileManagerJSON();
+          
+            // Create sample file; replace if exists.
+            Windows.Storage.StorageFolder storageFolder =
+                Windows.Storage.ApplicationData.Current.LocalFolder;
+            Windows.Storage.StorageFile sampleFile =
+                 await storageFolder.CreateFileAsync("apikeys.json", Windows.Storage.CreationCollisionOption.ReplaceExisting);
+            await Windows.Storage.FileIO.WriteTextAsync(sampleFile, json);
+
+            string text = await Windows.Storage.FileIO.ReadTextAsync(sampleFile);
+
+            Debug.WriteLine(text);
+        }
+        public class Keys
+        {
+            public string apikey;
+            public string secretkey;
         }
     }
 }
